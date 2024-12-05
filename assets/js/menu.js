15a00aff5a612e9283c4c486f43617b6321aaa27
Muradovvv <muradovv.murad2706@gmail.com>
2024-12-05 12:17:13 +0400

    murad

---
+++ assets/js/menu.js
@@ -0,0 +1,5 @@
+const menuIconBtn = document.querySelector(".menu-icon");
+const ulElem = document.querySelector("ul")
+menuIconBtn.addEventListener("click", function(){
+    ulElem.classList.toggle("show")
+});
